7343b69bc37866bdf64d1ca23d07035874d985c4
wtspcrvtt <lenya.stepanov.07@mail.ru>
2026-05-03 14:15:05 +0300

    work messanger with 1 window

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -7,6 +7,21 @@ import { auth } from "../firebase";
 function Chat() {
     const [inputText, setInputText] = useState('');
     const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const q = query(collection(db, 'messages'), orderBy('createdAt', 'asc'));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const msgs = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            setMessages(msgs);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    
+
     const sendMessage = async () => {
         if (inputText.trim() === "") return;
         try {
@@ -24,6 +39,11 @@ function Chat() {
     return (
         <>
         <div>Чат работает</div>
+        <div>{messages.map(msg => (
+            <div key={msg.id}>
+                <strong>{msg.senderId}</strong>: {msg.text}
+            </div>
+        ))}</div>
         <input type="text" value={inputText} onChange={(e) => setInputText(e.target.value)}/>
         <button type="button" onClick={sendMessage}>Отправить</button>
         </>   
